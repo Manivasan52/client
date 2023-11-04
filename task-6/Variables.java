f5ac3909cbb1493cb4b5ae6b014047caa1fe8a9b
Manivasan52 <manivasan.sightspectrum@gmail.com>
2023-11-04 14:12:29 +0530

    first commit

---
+++ task-6/Variables.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Variables {
+    public static void main(String[] args) {
+        int a=10;
+        String name="manivasan";
+        char c='a';
+        byte b=103;
+        boolean b1=true;
+        System.out.println("integer"+a);
+        System.out.println("String "+name);
+        System.out.println("character"+c);
+        System.out.println("byte"+b);
+        System.out.println("boolean"+b1);
+
+
+    }
+}
